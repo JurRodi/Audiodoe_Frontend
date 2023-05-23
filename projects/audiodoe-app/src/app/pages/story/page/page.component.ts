4e6be050c394b985c94d7ef761e27139961b496c
JurRodi <rodijurrien@gmail.com>
2023-05-23 16:16:51 +0200

    Feat: add display page controls

--- projects/audiodoe-app/src/app/pages/story/page/page.component.ts
+++ projects/audiodoe-app/src/app/pages/story/page/page.component.ts
@@ -3,21 +3,27 @@ import { PageModel } from '../../../api-client/models/page/pageModel'
 import { PageService } from '../services/page.service'
 import { Subscription } from 'rxjs'
 import { EPageType } from '../../../api-client/models/page/pageTypes'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
-  providers: [PageService],
 })
 export class PageComponent implements OnInit, OnDestroy {
+  private storyId = this.route.snapshot.paramMap.get('id') || ''
+  public pageNumber = this.route.snapshot.paramMap.get('pageNumber') || ''
+
   public page: PageModel | null = null
   public isLoading = false
   public pageType = EPageType
 
   public subscriptions: Subscription[] = []
 
-  constructor(private pageService: PageService) {}
+  constructor(
+    private pageService: PageService,
+    private route: ActivatedRoute
+  ) {}
 
   async ngOnInit(): Promise<void> {
     this.subscriptions.push(
@@ -29,7 +35,7 @@ export class PageComponent implements OnInit, OnDestroy {
         this.isLoading = isLoading
       })
     )
-    await this.pageService.getPage()
+    await this.pageService.getPage(this.storyId, this.pageNumber)
   }
 
   ngOnDestroy(): void {
--- projects/audiodoe-app/src/app/pages/story/services/page.service.ts
+++ projects/audiodoe-app/src/app/pages/story/services/page.service.ts
@@ -1,29 +1,21 @@
 import { Injectable } from '@angular/core'
 import { PageControllerService } from '../../../api-client/services/page/page-controller.service'
-import { ActivatedRoute } from '@angular/router'
 import { BehaviorSubject } from 'rxjs'
 import { PageModel } from '../../../api-client/models/page/pageModel'
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class PageService {
-  private storyId = this.route.snapshot.paramMap.get('id') || ''
-  private pageNumber = this.route.snapshot.paramMap.get('pageNumber') || ''
-
   public readonly page$ = new BehaviorSubject<PageModel | null>(null)
   public readonly pageLoading$ = new BehaviorSubject<boolean>(false)
 
-  constructor(
-    private pageController: PageControllerService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private pageController: PageControllerService) {}
 
-  public async getPage(): Promise<void> {
+  public async getPage(storyId: string, pageNumber: string): Promise<void> {
     this.pageLoading$.next(true)
 
-    const res: any = await this.pageController.getPage(
-      this.storyId,
-      this.pageNumber
-    )
+    const res: any = await this.pageController.getPage(storyId, pageNumber)
 
     this.page$.next(res)
     this.pageLoading$.next(false)
