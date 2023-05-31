b344012fedd91b255a6ed37c368a513b40cde2b0
Marianne <marianneneuts@gmail.com>
2023-05-31 07:59:35 +0200

    Feat: add latest changes

---
+++ projects/back-office/src/app/api-client/services/models/story/storyModel.ts
@@ -0,0 +1,11 @@
+export interface StoryModel {
+  _id: string
+  title: string
+  thumbnail: string
+  backGroundColor: string
+  description: string
+  duration: number
+  ageGroup: string
+  pageCount: number
+  category: string
+}
