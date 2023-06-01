f10c9fe116c24643cfcf9f259ec68790a08e074a
JurRodi <rodijurrien@gmail.com>
2023-06-01 15:25:04 +0200

    Feat: add authorization & generic styles & environment

--- projects/back-office/src/app/app-routing.module.ts
+++ projects/back-office/src/app/app-routing.module.ts
@@ -1,13 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AuthGuard } from './authentication/auth.guard'
 
 const routes: Routes = [
-  { path: 'story', loadChildren: () => import('./pages/story/story.module').then(m => m.StoryModule) },
-  { path: '**', redirectTo: '/', pathMatch: 'full' },
-];
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/authorization/authorization.module').then(
+        (m) => m.AuthorizationModule
+      ),
+  },
+  {
+    path: 'story',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./pages/story/story.module').then((m) => m.StoryModule),
+  },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- projects/back-office/src/app/app.module.ts
+++ projects/back-office/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoryComponent } from './pages/story/story.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { StoryComponent } from './pages/story/story.component'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
+import { AuthenticationInterceptor } from './authentication/authentication.interceptor'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StoryComponent,
+  declarations: [AppComponent, StoryComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthenticationInterceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
