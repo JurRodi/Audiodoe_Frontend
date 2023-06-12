a71ff6456df92584577c493c8b51b5f3706f4c45
JurRodi <rodijurrien@gmail.com>
2023-06-12 16:18:17 +0200

    Feat: add upload animation & position

--- projects/back-office/src/app/api-client/models/page/pageModel.ts
+++ projects/back-office/src/app/api-client/models/page/pageModel.ts
@@ -4,11 +4,11 @@ export interface PageModel {
   pageNumber: number
   backgroundImage: string | null
   backgroundColor: string | null
-  animations: string[] | null
+  animations: Animation[] | null
   choicePath: string
   choiceQuestion: string | null
   choices: string[] | null
-  choiceImages: string[] | null
+  choiceImages: string[] | []
   choiceSplit: boolean
   instructionsTitle: string | null
   audio: string
@@ -17,4 +17,11 @@ export interface PageModel {
   storyId: string
 
   audioFileName?: string
+  animationFileName?: string
+  hasPageNumberChanged?: boolean
+}
+
+export interface Animation {
+  fileName: string
+  position: string | null
 }
--- projects/back-office/src/app/pages/story/create-pages/edit-page/edit-page.component.ts
+++ projects/back-office/src/app/pages/story/create-pages/edit-page/edit-page.component.ts
@@ -11,7 +11,10 @@ import {
 } from 'firebase/storage'
 import { environment } from 'projects/back-office/src/environments/environment'
 import { StoryModel } from 'projects/back-office/src/app/api-client/models/story/storyModel'
-import { PageModel } from 'projects/back-office/src/app/api-client/models/page/pageModel'
+import {
+  PageModel,
+  Animation,
+} from 'projects/back-office/src/app/api-client/models/page/pageModel'
 
 @Component({
   selector: 'app-edit-page',
@@ -21,11 +24,22 @@ import { PageModel } from 'projects/back-office/src/app/api-client/models/page/p
 export class EditPageComponent {
   @Input() public story?: StoryModel
 
+  public pages = this.createStoryService.pages$.value
   public page: PageModel | null = null
   public text = new FormControl('')
-  public audio = new FormControl('')
+
   public audioFile: File | null = null
   public hasAudio = false
+
+  public animationFile: File | null = null
+  public hasAnimation = false
+  public initAnimation: Animation = {
+    fileName: '',
+    position: null,
+  }
+  public animation = structuredClone(this.initAnimation)
+
+  public animationPosition = new FormControl('')
 
   private app = initializeApp(environment.firebaseConfig)
   private storage = getStorage(this.app)
@@ -36,8 +50,16 @@ export class EditPageComponent {
     this.createStoryService.activePage$.subscribe((activePage) => {
       this.page = this.createStoryService.pages$.value[activePage]
       this.text.setValue(this.page?.text)
-      this.audio.setValue(null)
       this.page?.audio ? (this.hasAudio = true) : (this.hasAudio = false)
+      if (this.page?.animations?.length) {
+        this.hasAnimation = true
+        this.animation = this.page.animations[0]
+        this.animationPosition.setValue(this.animation.position)
+      } else {
+        this.hasAnimation = false
+        this.animation = structuredClone(this.initAnimation)
+        this.animationPosition.setValue('')
+      }
     })
   }
 
@@ -59,10 +81,10 @@ export class EditPageComponent {
     this.page.audioFileName = filename
     const filePath = this.story.title + '/audio/' + filename
     const fileRef = ref(this.storage, filePath)
-    await this.uploadFile(fileRef, this.audioFile)
+    await this.uploadAudio(fileRef, this.audioFile)
   }
 
-  public async uploadFile(
+  public async uploadAudio(
     fileRef: StorageReference,
     file: File
   ): Promise<void> {
@@ -78,5 +100,46 @@ export class EditPageComponent {
 
   public resetAudioUpload(): void {
     this.hasAudio = false
+  }
+
+  public onAnimationSelected(event: any): void {
+    this.animationFile = event.target.files[0]
+  }
+
+  public onAnimationUpload(): void {
+    if (!this.animationFile || !this.story || !this.page) return
+    this.page.animationFileName = this.animationFile.name
+    const filePath =
+      this.story.title + '/animation/' + this.page.animationFileName
+    const fileRef = ref(this.storage, filePath)
+    this.uploadAnimation(fileRef, this.animationFile)
+  }
+
+  public async uploadAnimation(
+    fileRef: StorageReference,
+    file: File
+  ): Promise<void> {
+    await uploadBytes(fileRef, file)
+    await getDownloadURL(fileRef).then((url) => {
+      this.animation.fileName = url
+      this.page!.animations = [this.animation]
+    })
+    this.createStoryService.pages$.value[
+      this.createStoryService.activePage$.value
+    ] = this.page!
+    this.hasAnimation = true
+  }
+
+  public resetAnimationUpload(): void {
+    this.hasAnimation = false
+  }
+
+  public selectedPosition(event: any): void {
+    this.animation.position = event.target.value
+    this.page!.animations = [this.animation]
+    this.createStoryService.pages$.value[
+      this.createStoryService.activePage$.value
+    ] = this.page!
+    console.log(this.createStoryService.pages$.value)
   }
 }
